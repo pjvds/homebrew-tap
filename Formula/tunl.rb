d6c678ac94571e3177945845e1ca53148b2803a6
Pieter Joost van de Sande <pj@born2code.net>
2021-01-20 20:23:23 +0100

    Brew formula update for tunl version 0.27.0

--- Formula/tunl.rb
+++ Formula/tunl.rb
@@ -5,25 +5,25 @@
 class Tunl < Formula
   desc "With Tunl you can expose services on your localhost to the public via a fast and secure tunnel."
   homepage "https://tunl.es/"
-  version "0.26.2"
+  version "0.27.0"
   license "MIT"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/pjvds/tunl/releases/download/0.26.2/tunl_0.26.2_Darwin_x86_64.tar.gz"
-    sha256 "ff35cf48e82fc581f5ca10d790e3e1be10e26ba79aecd2daf38ae5d22c8d487c"
+    url "https://github.com/pjvds/tunl/releases/download/0.27.0/tunl_0.27.0_Darwin_x86_64.tar.gz"
+    sha256 "cdbaa1c3eeeb764581032215d948116f3713f7be08ea274bc65ca60b60d241f8"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/pjvds/tunl/releases/download/0.26.2/tunl_0.26.2_Linux_x86_64.tar.gz"
-    sha256 "7037895c69a90c6a504c064d51b2157bc06b7969edb8fb658b5b8193fd179ba6"
+    url "https://github.com/pjvds/tunl/releases/download/0.27.0/tunl_0.27.0_Linux_x86_64.tar.gz"
+    sha256 "e331f8ed59968c494888721bd578d5b08ebb7d20ebd4ac35f5ad573c70702867"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/pjvds/tunl/releases/download/0.26.2/tunl_0.26.2_Linux_armv6.tar.gz"
-    sha256 "911852fb374417c259e712cbf4a4c56af65eab13f8cc88231168d776443c2bbf"
+    url "https://github.com/pjvds/tunl/releases/download/0.27.0/tunl_0.27.0_Linux_armv6.tar.gz"
+    sha256 "717431547efcf0619da82fcf7f179a5d05c648bc79e90588a7d4ee692bcfc872"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/pjvds/tunl/releases/download/0.26.2/tunl_0.26.2_Linux_arm64.tar.gz"
-    sha256 "df293aa00eccf37c4373ca6bc5997819fbb85474af100b915035dc101502d36f"
+    url "https://github.com/pjvds/tunl/releases/download/0.27.0/tunl_0.27.0_Linux_arm64.tar.gz"
+    sha256 "d01166a6a19df8b34692004d9fb8ff4962a28ee3172f6cfe8c5310a8449fd546"
   end
 
   def install
